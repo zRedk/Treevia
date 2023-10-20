898f8d6cdaaf4dd9577098af64f9d7a6e806b7bb
Emanuele-web04 <emanueledipietro612@gmail.com>
2023-10-20 16:12:51 +0200

    i added the leaf.swift files in order to create a foreach for everyleaf with an hstack in order to facilitate the communication between the wrong answers and the leaves

---
+++ MyLittleTree/Models/Leaf.swift
@@ -0,0 +1,13 @@
+//
+//  Leaf.swift
+//  MyLittleTree
+//
+//  Created by Emanuele Di Pietro on 20/10/23.
+//
+
+import Foundation
+
+struct Leaf: Identifiable {
+    var id = UUID()
+    var show = true
+}
---
+++ MyLittleTree/Models/LeafesView.swift
@@ -0,0 +1,16 @@
+//
+//  LeafesView.swift
+//  MyLittleTree
+//
+//  Created by Emanuele Di Pietro on 20/10/23.
+//
+
+import Foundation
+
+class LeavesView {
+    var leaves: [Leaf] = [
+        Leaf(),
+        Leaf(),
+        Leaf()
+    ]
+}
