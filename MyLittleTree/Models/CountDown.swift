afc81d4dee045f9b8b667db8ceaf6360a3d48a6a
Emanuele-web04 <emanueledipietro612@gmail.com>
2023-10-23 17:54:33 +0200

    Timer

---
+++ MyLittleTree/Models/CountDown.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MyLittleTree
+//
+//  Created by Emanuele Di Pietro on 23/10/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ MyLittleTree/Models/TimerViewModal.swift
@@ -0,0 +1,18 @@
+//
+//  TimerViewModal.swift
+//  MyLittleTree
+//
+//  Created by Emanuele Di Pietro on 23/10/23.
+//
+
+import SwiftUI
+
+struct TimerViewModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerViewModal()
+}
